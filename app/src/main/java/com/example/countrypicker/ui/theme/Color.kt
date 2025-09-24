67d85a6c134d9a19ca428b5d90f4313c7501fe42
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-24 17:49:15 +0300

    add themes, colors and typographies

---
+++ app/src/main/java/com/example/countrypicker/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.countrypicker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val ButtonColor = Color(0xFF5F41DF)
+val PrimaryText = Color(0xFF120934)
+val SecondaryText = Color(0xFF8D8B8C)
+val LinkText = Color(0xFF76BAE0)
+val LightGray = Color(0xFFD8D8D8)
+
